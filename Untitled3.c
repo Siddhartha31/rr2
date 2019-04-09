f4681c9845c2a4647b193340d3ea0ad4b219c30c
Siddhartha31 <42816605+Siddhartha31@users.noreply.github.com>
2019-04-10 00:46:35 +0530

    Update Untitled3.c

--- Untitled3.c
+++ Untitled3.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-struct process_data
+#include<stdio.h> 
+ struct process_data
 {
 	int Num;
 	int Pid;  //Process Id
@@ -11,31 +11,59 @@ struct process_data
 	int W_time; //Waiting Time
 	int S_time; //Process start Time
 	int Res_time;
+	int status;
 
 };
-int main()
-{
-	struct process_data a[6];
-	int i;
-	for(i=0;i<6;i++)
-	{
-		printf("enter the process id,arrival time ,priority,burst time ");
-		scanf("%d %d %d",&a[i].Pid,&a[i].Priority,&a[i].A_time,&a[i].B_time);
-	}
-	struct process_data temp;
-int j;
-for (i=0;i<9;i++)
-{
-	for(j=0;j<9-i;j++)
-	{
-		if(a[j].A_time<a[j+1].A_time)
-		{
-			temp=a[j];
-			a[j]=a[j+1];
-			a[j+1]=temp;
-		}
-	}
-	
-}
-	//sorted according to the arrival time and then apply rr using priroirty
+int main() 
+{ 
+ 
+  int cnt,j,n,time,remain_process,flag=0,tq; 
+  int w_time=0,t_time=0,arrivaltime[10],bursttime[10],remaintime[10]; 
+  printf("Enter Total Process:\t "); 
+  scanf("%d",&n); 
+  struct process_data a[n];
+  remain_process=n; 
+  for(cnt=0;cnt<n;cnt++) 
+  { a[cnt].Pid=cnt+1;
+    printf("Enter Arrival Time ,Burst Time  ,prirority  for Process Process Number %d :",cnt+1); 
+    scanf("%d",&a[cnt].A_time); 
+    scanf("%d",&a[cnt].B_time); 
+    scanf("%d",&a[cnt].Priority);
+    a[cnt].R_time=a[cnt].B_time; 
+  } 
+  printf("Enter Time Quantum:\t"); 
+  scanf("%d",&tq); 
+  printf("\n\nProcess\t|Turnaround Time|Waiting Time|\t Arrival Time\n\n"); 
+  for(time=0,cnt=0;remain_process!=0;) 
+  { 
+    if( a[cnt].R_time<=tq &&  a[cnt].R_time>0) 
+    { 
+      time+= a[cnt].R_time; 
+      a[cnt].R_time=0; 
+      flag=1; 
+    } 
+    else if( a[cnt].R_time>0) 
+    { 
+       a[cnt].R_time-=tq; 
+      time+=tq; 
+    } 
+    if( a[cnt].R_time==0 && flag==1) 
+    { 
+      remain_process--; 
+      printf("P[%d]\t|\t%d\t|\t%d\t %d\n",cnt+1,time-a[cnt].A_time,time-a[cnt].A_time-a[cnt].B_time,a[cnt].A_time); 
+      w_time+=time-a[cnt].A_time-a[cnt].B_time; 
+      t_time+=time-a[cnt].A_time; 
+      flag=0; 
+    } 
+    if(cnt==n-1) 
+      cnt=0; 
+    else if(a[cnt+1].A_time<=time) 
+      cnt++; 
+    else 
+      cnt=0; 
+  } 
+  printf("\nAverage Waiting Time= %f\n",w_time*1.0/n); 
+  printf("Avg Turnaround Time = %f",t_time*1.0/n); 
+  
+  return 0; 
 }
