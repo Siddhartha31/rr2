857990de732bd3b3acdfd12f8866ff6257aa6f8a
Siddhartha31 <42816605+Siddhartha31@users.noreply.github.com>
2019-04-10 23:10:23 +0530

    Update Untitled3.c

--- Untitled3.c
+++ Untitled3.c
@@ -1,5 +1,6 @@
-#include<stdio.h> 
- struct process_data
+#include<stdio.h>
+#include<conio.h>
+struct process_data
 {
 	int Num;
 	int Pid;  //Process Id
@@ -7,63 +8,98 @@
 	int B_time; //Process Burst Time
 	int Priority; //Process Priority
 	int F_time; //Process Finish Time
-	int R_time; //Process Remaining  Time During Execution
+	int t_time; //Process Remaining  Time During Execution
 	int W_time; //Waiting Time
 	int S_time; //Process start Time
 	int Res_time;
 	int status;
 
 };
-int main() 
-{ 
+int main()
+ {
+   int n,awt,atat,i,tq;
  
-  int cnt,j,n,time,remain_process,flag=0,tq; 
-  int w_time=0,t_time=0,arrivaltime[10],bursttime[10],remaintime[10]; 
-  printf("Enter Total Process:\t "); 
-  scanf("%d",&n); 
-  struct process_data a[n];
-  remain_process=n; 
-  for(cnt=0;cnt<n;cnt++) 
-  { a[cnt].Pid=cnt+1;
-    printf("Enter Arrival Time ,Burst Time  ,prirority  for Process Process Number %d :",cnt+1); 
-    scanf("%d",&a[cnt].A_time); 
-    scanf("%d",&a[cnt].B_time); 
-    scanf("%d",&a[cnt].Priority);
-    a[cnt].R_time=a[cnt].B_time; 
-  } 
-  printf("Enter Time Quantum:\t"); 
-  scanf("%d",&tq); 
-  printf("\n\nProcess\t|Turnaround Time|Waiting Time|\t Arrival Time\n\n"); 
-  for(time=0,cnt=0;remain_process!=0;) 
-  { 
-    if( a[cnt].R_time<=tq &&  a[cnt].R_time>0) 
-    { 
-      time+= a[cnt].R_time; 
-      a[cnt].R_time=0; 
-      flag=1; 
-    } 
-    else if( a[cnt].R_time>0) 
-    { 
-       a[cnt].R_time-=tq; 
-      time+=tq; 
-    } 
-    if( a[cnt].R_time==0 && flag==1) 
-    { 
-      remain_process--; 
-      printf("P[%d]\t|\t%d\t|\t%d\t %d\n",cnt+1,time-a[cnt].A_time,time-a[cnt].A_time-a[cnt].B_time,a[cnt].A_time); 
-      w_time+=time-a[cnt].A_time-a[cnt].B_time; 
-      t_time+=time-a[cnt].A_time; 
-      flag=0; 
-    } 
-    if(cnt==n-1) 
-      cnt=0; 
-    else if(a[cnt+1].A_time<=time) 
-      cnt++; 
-    else 
-      cnt=0; 
-  } 
-  printf("\nAverage Waiting Time= %f\n",w_time*1.0/n); 
-  printf("Avg Turnaround Time = %f",t_time*1.0/n); 
-  
-  return 0; 
+   //n is number of process
+   //p is process
+   //a[i].Priority is process priority
+   //a[i].B_time is process burst time
+   //w is wait time
+   // t is turnaround time
+   //awt is average waiting time
+   //atat is average turnaround time
+   //at is arrival time
+   //tq is time qunatum
+ 
+ 
+   printf("Enter the number of process : ");
+   scanf("%d",&n);
+   struct process_data a[n],x;
+   printf("Enter time quantum");
+   scanf("%d",&tq);
+   printf("\n\t Enter burst time : time priorities : Arrival time  \n");
+ 
+   for(i=0;i<n;i++)
+    {
+      
+      printf("\n Process %d ",i+1);
+      
+      scanf("%d %d %d",&a[i].B_time,&a[i].Priority,&a[i].A_time);
+	  a[i].Pid=i+1;
+    }
+ 
+//sorting on the basis of priority
+int j;
+  for(i=0;i<n-1;i++)
+   {
+     for(j=i+1;j<n;j++)
+     {
+       if(a[i].Priority>a[j].Priority)
+       {
+     x=a[i];
+     a[i]=a[j];
+     a[i]=x;
+     
+      }
+      
+   }
+}
+a[i].W_time=0;
+awt=0;
+a[0].t_time=a[0].B_time;
+atat=a[0].t_time;
+for(i=1;i<n;i++)
+ {
+ 	if(tq<n || tq>n) {
+	 
+   a[i].W_time=a[i-1].t_time;
+   awt+=a[i].W_time;
+   a[i].t_time=a[i].W_time+a[i].B_time;
+   atat+=a[i].t_time;
+	 }
+ }
+ 
+ //Gantt chrat
+ printf("\n");
+ printf("Gantt chart\n");
+ for(i=0;i<n;i++)
+ {
+ 	printf("P %d ",a[i].Pid);
+ }
+//Displaying the process
+ 
+printf("\n\nProcess \t Burst Time \t Wait Time \t Turn Around Time   Priority \tArrival time \n");
+for(i=0;i<n;i++){
+printf("\n  %d",a[i].Pid); printf("\t\t %d",a[i].B_time); printf("\t\t %d",a[i].W_time); printf("\t\t %d",a[i].t_time); 
+printf("\t\t %d",a[i].Priority); printf("\t\t %d",a[i].A_time);
+
+}
+int num=105/15;
+printf("\n cpu utilization rate %d",num);
+awt/=n;
+atat/=n;
+printf("\n Average Wait Time : %d ",awt);
+printf("\n Average Turn Around Time : %d",atat);
+
+
+getch();
 }
